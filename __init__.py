ebcc9cc7c8ce147d1fea52806eb609ea66532651
Russell Anderson <5637107+rpanderson@users.noreply.github.com>
2016-08-22 09:50:04 +1000

    Implements proposal of issue #10, allowing Allow check_version to require only minimum version.

--- __init__.py
+++ __init__.py
@@ -11,20 +11,28 @@
 #                                                                   #
 #####################################################################
 
-__version__ = '2.2.0'
+__version__ = '2.3.0'
 
 
 class VersionException(Exception):
     pass
 
     
-def check_version(module_name, at_least, less_than, version=None):
+def check_version(module_name, at_least, less_than=None, version=None):
 
     from distutils.version import LooseVersion
 
     if version is None:
         version = __import__(module_name).__version__
-    at_least_version, less_than_version, installed_version = [LooseVersion(v) for v in [at_least, less_than, version]]
-    if not at_least_version <= installed_version < less_than_version:
-        raise VersionException(
-            '{module_name} {version} found. {at_least} <= {module_name} < {less_than} required.'.format(**locals()))
+    # Require at_least version or later
+    if less_than is None:
+        at_least_version, installed_version = [LooseVersion(v) for v in [at_least, version]]
+        if not at_least_version <= installed_version:
+            raise VersionException(
+                '{module_name} {version} found. {at_least} <= {module_name} required.'.format(**locals()))
+    # Require at_least version but before less_than
+    else:
+        at_least_version, less_than_version, installed_version = [LooseVersion(v) for v in [at_least, less_than, version]]
+        if not at_least_version <= installed_version < less_than_version:
+            raise VersionException(
+                '{module_name} {version} found. {at_least} <= {module_name} < {less_than} required.'.format(**locals()))
